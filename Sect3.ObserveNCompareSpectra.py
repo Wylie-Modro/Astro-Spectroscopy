b8ce2bb3507bb0ce5d9ff214d13f1b092b8ff028
William Wylie-Modro <williamwyliemodro@gmail.com>
2017-02-18 17:14:06 -0800

    bit more on Sect3

--- Sect3.ObserveNCompareSpectra.py
+++ Sect3.ObserveNCompareSpectra.py
@@ -10,21 +10,21 @@ allIncandescentBulb = LoadData.LoadTextFromDirectoryIntoArray('incandescentBulb'
 allFluoroscentBulb = LoadData.LoadTextFromDirectoryIntoArray('fluoroscentBulb', '2017Lab2GroupB/fluoroscentBulb/')
 allH2GDL = LoadData.LoadTextFromDirectoryIntoArray('H2GDL', '2017Lab2GroupB/H2GDL/')
 
-AveragedIncandescentBulb = DataTools.GetAveragedImage(allSpectrasIncandescentBulb, 2048)
+averagedIncandescentBulb = DataTools.GetAveragedImage(allIncandescentBulb, 2048)
+averagedFluoroscentBulb = DataTools.GetAveragedImage(allFluoroscentBulb , 2048)
 
 
-DataTools.SpectraPlot(AveragedIncandescentBulb, plt, 'AveragedIncandescentBulb', 'Pixel (Number)', 'Intensity (Counts)')
 
-'''
+
+fig = plt.figure()
+
 ax1 = fig.add_subplot(211)
 ax2 = fig.add_subplot(212)
 
-DataTools.SpectraPlot(singleSpectra, ax1, 'incandescentBulb1', 'Pixel (Number)', 'Intensity (Counts)')
+DataTools.SpectraPlot(averagedIncandescentBulb, ax1, 'AveragedIncandescentBulb', 'Pixel (Number)', 'Intensity (Counts)')
+DataTools.SpectraPlot(averagedIncandescentBulb, ax2, 'AveragedFluoroscentBulb', 'Pixel (Number)', 'Intensity (Counts)')
 
-DataTools.SpectraPlot(singleSpectra2, ax2, 'incandescentBulb2', 'Pixel (Number)', 'Intensity (Counts)')
-'''
 
-fig = plt.figure()
 
 plt.show()
 
