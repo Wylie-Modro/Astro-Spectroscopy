4a351f23c8de16807f2e47171ffabb28a67d4940
William Wylie-Modro <williamwyliemodro@gmail.com>
2017-02-20 10:03:00 -0800

    Sect5 PartA Complete: Determine the wavelength calibration of the spectrometer

--- Sect5DetermineWavelengthCalibration.py
+++ Sect5DetermineWavelengthCalibration.py
@@ -1,11 +1,44 @@
-def SimplePixlToWLMapping(pixelValue):
-    a0 = 339.672754
-    a1 = 0.38194
-    a2 = -0.0000185296
-    a3 = -0.00000000197619
-    WL = a0+a1*(pixelValue)+a2*(pixelValue**2)+a3*(pixelValue**3)
-    return WL
+import numpy as np
+import matplotlib.pyplot as plt
+import SpectraClasses as SpectraCls
+
+#Part A: Determine the wavelength calibration of the spectrometer, i.e., the mapping between pixel number and wavelength
+#-----------------------------------------------------------------------------------------------------------------------
+#Initialize classes
+LoadData = SpectraCls.LoadingData()
+DataTools = SpectraCls.DataTools()
+CentroidAl = SpectraCls.CentroidAlgorithm()
     
-    
-print (SimplePixlToWLMapping(2048.0))
-    
+#Load Data and get averaged-spectra
+allHeGDL = LoadData.LoadTextFromDirectoryIntoArray('HeGDL', '2017Lab2GroupB/HeGDL/')
+averagedHeGDL = DataTools.GetAveragedImage(allHeGDL , 2048)
+
+#Create dict and convert keys to wavelength outputed as np.array
+HeGDLDictPixel = DataTools.CreateDictOfSpectra(averagedHeGDL)
+
+HeGDLWLList = []
+for key, val in HeGDLDictPixel.items():
+    HeGDLWLList.append([CentroidAl.SimplePixlToWLMapping(key), val])
+HeGDLWLnpArr = np.array(HeGDLWLList)  
+
+
+#Setup plots comparing pixel number and wavelength representations
+fig = plt.figure()     
+ax1 = fig.add_subplot(211)
+ax2 = fig.add_subplot(212)
+DataTools.SpectraPlot(averagedHeGDL, ax1, 'Averaged HeGDL (Pixel)', 'Pixel (Number)', 'Intensity (Counts)')
+DataTools.SpectraPlot(HeGDLWLnpArr, ax2, 'Averaged HeGDL (with simple WL Conversion)', 'Wavelength (nm)', 'Intensity (Counts)')
+
+plt.show()
+#-----------------------------------------------------------------------------------------------------------------------
+
+#Part B: Use the method of linear least squares to determine a polynomial fit to these centroid data to derive the wavelength solution.
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+
+
+
+
+
--- SpectraClasses.py
+++ SpectraClasses.py
@@ -253,6 +253,15 @@ class CentroidAlgorithm():
         print('Number of Emission lines: ' + str(len(listOfTrailingPoints)))
         #print('listOfTrailingPoints: ' + str(listOfTrailingPoints))
         return listOfTrailingPoints
+    
+    @staticmethod
+    def SimplePixlToWLMapping(pixelValue):
+        a0 = 339.672754
+        a1 = 0.38194
+        a2 = -0.0000185296
+        a3 = -0.00000000197619
+        WL = a0+a1*(pixelValue)+a2*(pixelValue**2)+a3*(pixelValue**3)
+        return WL
             
             
 class Distributions:
