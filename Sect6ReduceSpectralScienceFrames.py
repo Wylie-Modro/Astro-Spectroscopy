bd627a492844431be0bcce7b389cf1492cfc76e5
Mudit Garodia <mgarodia@ucsd.edu>
2017-02-21 01:50:05 -0800

    More on Sect 6, KAST fits files

--- Sect6ReduceSpectralScienceFrames.py
+++ Sect6ReduceSpectralScienceFrames.py
@@ -6,9 +6,39 @@ import matplotlib.pyplot as plt
 import SpectraClasses as SpectraCls
 import pyfits
 
-image = pyfits.open('data-2013-10-26-shane-public/b153.fits')
-imagehead = image[0].header
-print imagehead['EXPTIME']
+image = pyfits.open('data-2013-10-26-shane-public/biasData/b101.fits')
+#print('image: '+str(type(image)))
+#print('image: '+str(image))
+#imagehead = image[0].header
+#print imagehead['EXPTIME']
 
 imagedata = image[0].data
-print(type(imagedata))
+for i in imagedata:
+    print(i)
+print(len(imagedata))
+
+LoadData = SpectraCls.LoadingData()
+DataTools = SpectraCls.DataTools()
+'''
+allBiasData = LoadData.LoadTextFromDirectoryIntoArray('b', 'data-2013-10-26-shane-public/biasData/')
+
+
+        listOfData = []
+
+        print("Loading in images ...")
+
+        for filename in os.listdir(directoryPath):
+    
+            if commonStartOfFileName in str(filename):
+                data = np.loadtxt(directoryPath+filename, skiprows=17, comments = '>>', usecols=(0,1))
+                listOfData.append(data)
+            else:
+                print('Found: ' + str(filename))
+    
+        return listOfData
+
+averagedBiasData = DataTools.GetAveragedImage(allBiasData, 2048)
+
+fig = plt.figure()
+plt.show()
+'''
